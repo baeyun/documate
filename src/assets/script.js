86d4e3f39a83a07458ddccb1c3c58ceafcbba406
bukharim96 <bukharim96@gmail.com>
2018-12-21 02:40:02 +0200

    Fetch partials from server

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,1 +1,13 @@
-console.log('HIIRAAN')
+document.querySelectorAll('#navbar a').forEach(function(el) {
+	el.onclick = function(e) {
+		e.preventDefault();
+
+		fetch(el.href, {method: 'GET'})
+		.then(function(res) {
+      return res.text();
+    })
+		.then(function(textResult) {
+			document.getElementById('main-content').innerHTML = textResult;
+		});
+	}
+});
