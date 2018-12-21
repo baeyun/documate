a2c58cedf6727fa99612caa1d421f993f3da1464
bukharim96 <bukharim96@gmail.com>
2018-12-21 02:07:09 +0200

    Checkpoint

--- src/index-template.js
+++ src/index-template.js
@@ -1,17 +1,31 @@
-module.exports = nav => `<!DOCTYPE html>
+const {
+  readFileSync
+} = require('fs')
+
+const styles = readFileSync('./src/assets/styles.css').toString()
+const script = readFileSync('./src/assets/script.js').toString()
+
+module.exports = (nav, defaultPartial = '') => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Documate</title>
+  <style>
+${styles}
+  </style>
 </head>
 <body>
   <nav id="navbar">
     ${nav}
   </nav>
-  <main>
-    
+  <main id="main-content">
+    ${defaultPartial}
   </main>
+
+  <script type="text/javascript">
+${script}
+  </script>
 </body>
 </html>`
--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
-const { readFileSync, writeFileSync } = require('fs')
+const {
+  readFileSync,
+  writeFileSync,
+  mkdirSync,
+  existsSync
+} = require('fs')
 const { exec } = require('child_process')
 
+const rimraf = require('rimraf')
+const uniqider = require('uniqider')
 const { Converter } = require('showdown')
 const htmlTemplate = require('./index-template')
-const converter = new Converter()
-const navContent = readFileSync('./documate/nav.md').toString()
-const nav = converter.makeHtml(navContent)
-const links = navContent.match(/^\s*\-\s*\[.+\]\((.+)\)\s*$/gim)
-  .map(link => link.match(/\((.+)\)/i)[1])
+const converterInstance = new Converter()
+let navContent = readFileSync('./documate/nav.md').toString()
+let initialPartial = ''
 
-console.log(links)
+if (!existsSync('./documate/public/partials/'))
+  mkdirSync('./documate/public/partials/')
+else
+  rimraf.sync(
+    './documate/public/partials/*'
+  )
 
-// writeFileSync('./documate/cache/index.html', htmlTemplate(nav))
+navContent.split('\n').filter(l => l.trim() !== '')
+.map((line, i) => {
+  // FIXME: `*` not appropriate
+  let link = line.match(/\([\.\/]*(.+)\)/i)
+  let id = uniqider()
+  let partial = converterInstance.makeHtml(
+    readFileSync('./documate/' + link[1]).toString()
+  )
+  
+  if (i === 0)
+    initialPartial = partial
 
-// const thread = exec(
-//   'parcel ./documate/cache/index.html --out-dir ./documate/public --no-cache'
-// )
+  writeFileSync(
+    `./documate/public/partials/${id}.html`,
+    partial
+  )
 
-// thread.stdout.on('data', data => {
-//   console.log(data.toString())
-// })
+  try {
+    navContent = navContent.replace(
+      line.trim(),
+      line.trim().split('](')[0] + `](../public/partials/${id}.html)`
+    )
+  } catch(e) {}
+})
 
-// thread.stderr.on('data', data => {
-//   console.log(data.toString())
-// })
+const nav = converterInstance.makeHtml(navContent)
 
-// thread.on('exit', code => {
-//   console.log(`Child exited with code ${code}`)
-// })
+writeFileSync(
+  './documate/cache/index.html',
+  htmlTemplate(nav, initialPartial)
+)
+
+const thread = exec(
+  'parcel ./documate/cache/index.html --out-dir ./documate/public --no-cache'
+)
+
+thread.stdout.on('data', data => {
+  console.log(data.toString())
+})
+
+thread.stderr.on('data', data => {
+  console.log(data.toString())
+})
+
+thread.on('exit', code => {
+  console.log(`Child exited with code ${code}`)
+})
