a285e8a94179d399b3632d74c21343b4fcf2ccb0
bukharim96 <bukharim96@gmail.com>
2018-12-29 03:47:03 +0200

    SITE: Minor fixes

--- src/init.js
+++ src/init.js
@@ -23,7 +23,7 @@ Hi, and welcome to Documate, the tool that'll make creating sites for your proje
 
 Start by editing this file in \`./documate/welcome.md\` in your project's root directory. Control your site routes from \`./documate/nav.md\`.
 
-For more info on getting started, visit our [docs](http://documate.github.io) that's been built with Documate too.
+For more info on getting started, visit our [docs](https://documate.netlify.com/) that's been built with Documate too.
 
 Enjoy writing docs with documate. Made with ❤ by [@bukharim96](https://twitter.com/bukharim96) and [@undefinedbuddy](https://twitter.com/undefinedbuddy).`)
 
