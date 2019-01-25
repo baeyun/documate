9e7cc11e491a900f9ce6eaac32089a5923c53896
bukharim96 <bukharim96@gmail.com>
2019-01-26 00:55:57 +0200

    Fix searchables global

--- scripts/utils.js
+++ scripts/utils.js
@@ -16,7 +16,7 @@ let markedRenderer = new marked.Renderer();
 markedRenderer.heading = (text, level) => {
   let escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
 
-  return `<h${level} id="${escapedText}">
+  return `<h${level} id="${escapedText}" data-title="${text}">
     <a href="#${escapedText}" aria-hidden="true" class="anchor"><svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>
     ${text}
   </h${level}>`;
@@ -136,12 +136,12 @@ module.exports = {
 
           // Extract searchable data
           // then append info to searchables array
-          let headers = htmlDocContent.match(/\<h[1-6].+?\<\/h[1-6]\>/gim);
+          let headers = htmlDocContent.match(/\<h[1-6]\sid\=\".+?\"\sdata-title\=\".+?\"\>/gim);
           if (headers)
             for (let i = 0; i < headers.length; i++) {
               let header = headers[i];
               let match = header.match(
-                /\<h[1-6]\sid\=\"(.+?)\"\>(.+?)\<\/h[1-6]\>/i
+                /\<h[1-6]\sid\=\"(.+?)\"\sdata-title\=\"(.+?)\"\>/i
               );
 
               if (!match) continue;
