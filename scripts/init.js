50dee7a60e053f71f4f77e9e08c7b27b54ad2204
bukharim96 <bukharim96@gmail.com>
2019-01-26 18:13:11 +0200

    Checkpoint

--- scripts/init.js
+++ scripts/init.js
@@ -1,9 +1,19 @@
 /**
- * documate
- *  public
- *  docs
- *  config.js
- *  index.html
- *  logo.png
- *  navs.md
+ * Documate create docs site files
  */
+
+const { existsSync, copySync } = require("fs-extra");
+const { normalize } = require("path");
+
+const { throwErr } = require("./utils");
+
+const CWD = process.cwd();
+
+if (!existsSync(CWD + "/package.json"))
+  throwErr("A package.json file is require to be present in project root.");
+
+// Copy temp files over to project
+copySync(
+  normalize(__dirname + "/temps/documate/"),
+  normalize(CWD + "/documate")
+);
