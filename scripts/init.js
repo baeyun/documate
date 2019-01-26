4340b74787a0885c7aea887c3794c4d204fd0c2e
bukharim96 <bukharim96@gmail.com>
2019-01-26 18:51:41 +0200

    Init complete

--- scripts/init.js
+++ scripts/init.js
@@ -3,17 +3,26 @@
  */
 
 const { existsSync, copySync } = require("fs-extra");
-const { normalize } = require("path");
-
 const { throwErr } = require("./utils");
+const chalk = require("chalk");
+const path = require("path");
 
 const CWD = process.cwd();
 
 if (!existsSync(CWD + "/package.json"))
   throwErr("A package.json file is require to be present in project root.");
 
+if (existsSync(CWD + "/documate/"))
+  throwErr(
+    "Delete the documate folder in root directory first before re-initializing."
+  );
+
 // Copy temp files over to project
-copySync(
-  normalize(__dirname + "/temps/documate/"),
-  normalize(CWD + "/documate")
+copySync(path.join(__dirname, "/temps/documate/"), path.join(CWD, "/documate"));
+
+// success
+console.log(
+  chalk.green(
+    "\n  Documate has been successfully initialized in this project.\n"
+  )
 );
